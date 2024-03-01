492f38f8fb815c91ad12f6346a03c0414097c2b4
trandanghuy <161806139+trandanghuy@users.noreply.github.com>
2024-03-01 13:35:21 +0700

    Add files via upload

---
+++ BaiTapNgay3.js
@@ -0,0 +1,11 @@
+//checksochan
+const check = (a) => a%2 === 0;
+
+// destructuring assignment
+
+/*const action = require('lib/action')
+const service = require('lib/service')*/
+ import {action, service} from 'lib';
+
+
+ //
